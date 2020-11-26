f18c68d64a29400890dd13e55ef703256a456d89
Emil98FI <ebrummer32@gmail.com>
2020-11-26 03:38:57 +0200

    done

--- imagegallery/src/App.js
+++ imagegallery/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css"
 import React from "react"
+import savedImages from "./components/SavedImages"
+import Footer from "./components/Footer"
 import ImageGallery from "./components/ImageGallery"
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 function App() {
   return (
-    <div className="main">
-      <ImageGallery />
-    </div>
+    <Router>
+      <div className="main">
+        <Switch>
+          <Route path="/" exact component={ImageGallery} />
+          <Route path="/saved" component={savedImages} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
--- imagegallery/src/components/SearchBar.js
+++ imagegallery/src/components/SearchBar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
+import Navbar from "react-bootstrap/Navbar"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 class SearchBar extends React.Component {
@@ -24,20 +25,52 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-      <Form onSubmit={this.props.onSubmit}>
-        <Form.Group controlId={this.props.controlId}>
-          <Form.Control
-            name={this.props.searchValue}
-            value={this.props.searchValue}
-            onChange={this.props.onChange}
-            placeholder="Search"
-          />
-        </Form.Group>
+      <div className="up">
+        <Form onSubmit={this.props.onSubmit}>
+          <Navbar
+            style={{ display: "flex", width: " 200%" }}
+            bg="dark"
+            variant="dark"
+          >
+            {this.props.isSaved === true ? (
+              <Navbar.Brand
+                href={this.props.href}
+                bg="light"
+                onClick={this.props.onClick}
+              >
+                Show Home
+              </Navbar.Brand>
+            ) : (
+              <Navbar.Brand href={this.props.href} onClick={this.props.onClick}>
+                Show My Saved Gallery
+              </Navbar.Brand>
+            )}
+          </Navbar>
 
-        <Button variant="primary" type="submit">
-          Search
-        </Button>
-      </Form>
+          <br />
+          {this.props.isSaved === true ? (
+            <h1>Saved Images</h1>
+          ) : (
+            <Form.Group controlId={this.props.controlId}>
+              <Form.Control
+                name={this.props.searchValue}
+                value={this.props.searchValue}
+                onChange={this.props.onChange}
+                placeholder="Search"
+              />
+            </Form.Group>
+          )}
+          {this.props.isSaved === true ? (
+            <Button variant="danger" onClick={this.props.deleteAll}>
+              Delete All
+            </Button>
+          ) : (
+            <Button variant="primary" type="submit">
+              Search
+            </Button>
+          )}
+        </Form>
+      </div>
     )
   }
 }
